f02e72b25f50d955707cb2b3cd7aa4ec45c8430a
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2017-08-28 14:37:23 -0300

    Adiciona gitignore e exercicios do cap2.

---
+++ MeuPrograma.java
@@ -0,0 +1,7 @@
+class MeuPrograma{
+	public static void main(String[] args){
+		System.out.println("Olá mundo");
+		System.out.println("Outra linha");
+		System.out.println("Duas linhas\n em uma linha só");
+	}
+}
